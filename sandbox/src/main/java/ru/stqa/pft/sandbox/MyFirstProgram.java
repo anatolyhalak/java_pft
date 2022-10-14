a50362d355bbb0ae978150797d71ac036d81a22c
scsi123 <scsi1234@yandex.ru>
2022-10-14 22:36:34 +0300

    Типы переменных и значений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-      System.out.println("Hello, world!");
+      String somebody = "world";
+      System.out.println("Hello, " + somebody + "!");
+
+      double l = 8.0;
+      double s = l * l;
+      System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 
 }
