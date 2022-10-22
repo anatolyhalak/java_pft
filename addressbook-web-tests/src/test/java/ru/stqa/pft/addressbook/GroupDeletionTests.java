9205cb88587a226540bab9ca5aa2330ee8097434
scsi123 <scsi1234@yandex.ru>
2022-10-22 13:20:55 +0300

    Заменяем наследование на делегирование
    - создан класс для делегирования applicationManager
    - в него перенесены все вспомогательные методы из TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -6,10 +6,10 @@ public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
-    goToGroupPage();
-    selectGroup();
-    deleteSelectedGroups();
-    returnToGroupPage();
+    app.goToGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroups();
+    app.returnToGroupPage();
   }
 
 }
