610221ee305538232e681b6ab54fdeb6161c0a48
scsi123 <scsi1234@yandex.ru>
2022-10-22 15:43:53 +0300

    Общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -40,14 +40,7 @@ public class ApplicationManager {
     }
   }
 
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
