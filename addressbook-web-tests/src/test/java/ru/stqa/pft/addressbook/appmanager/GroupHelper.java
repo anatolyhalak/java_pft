9d911f370871e040ebaa4b5795747d75a7120866
scsi123 <scsi1234@yandex.ru>
2022-10-24 22:53:24 +0300

    Создан вспомогательный метод нажатия кнопки на странице Редактирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -39,4 +39,8 @@ public class GroupHelper extends HelperBase {
   public void initGroupModification() {
     click(By.name("edit[]"));
   }
+
+  public void submitGroupModification() {
+    click(By.name("update[]"));
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupModificationTests extends TestBase {
 
@@ -9,5 +10,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
   }
 }
