272704df4f055bf88a8c90dca52858fa9915780a
scsi123 <scsi1234@yandex.ru>
2022-10-31 00:37:42 +0300

    Проверки в классе NavigationHelper
    - созданы проверки для почти одинаковых страниц

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-  public NavigationHelper(WebDriver wd){
+  public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 }
