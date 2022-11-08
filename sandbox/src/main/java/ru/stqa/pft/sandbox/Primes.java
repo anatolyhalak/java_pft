c265994772cf6eec8418697d3297e2d8d21b9b91
scsi123 <scsi1234@yandex.ru>
2022-11-08 23:30:53 +0300

    Использование For и While

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -10,4 +10,14 @@ public class Primes {
     }
     return true;
   }
+  public static boolean isPrimeWhile(int n) {
+    int i = 2;
+    while (i < n) {
+      if (n % i == 0) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -6,7 +6,11 @@ import org.testng.annotations.Test;
 public class PrimeTests {
 
   @Test
-  public void testPrimes() {
+  public void testPrime() {
     Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
   }
 }
