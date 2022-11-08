5aad92931f64de7210db0927975a030d4c9f8888
scsi123 <scsi1234@yandex.ru>
2022-11-08 23:47:11 +0300

    Созданы быстрые циклы, сокращено время итерации

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -7,7 +7,12 @@ public class PrimeTests {
 
   @Test
   public void testPrime() {
-    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+  @Test(enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
   }
   @Test
   public void testNonPrime() {
